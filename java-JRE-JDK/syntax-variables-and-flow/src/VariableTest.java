a6a9d5a7f14a46f776d70470e54f67cc80c958aa
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-06-11 20:54:34 -0300

    Tipo Inteiro: int

---
+++ java-JRE-JDK/syntax-variables-and-flow/src/VariableTest.java
@@ -0,0 +1,21 @@
+public class VariableTest {
+
+    public static void main(String[] args) {
+        System.out.println("Ola novo test");
+
+        int age;
+        age = 23;
+
+        System.out.println(age);
+
+        age = 30 + 10;
+        System.out.print(age); //Sem Enter
+
+        age = (7 * 5) + 2;
+        System.out.println(age);
+
+        age = 23;
+        System.out.println("A idade é: " + age + "!");
+    }
+
+}
