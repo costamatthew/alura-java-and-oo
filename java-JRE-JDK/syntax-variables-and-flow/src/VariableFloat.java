325fdf18dd6e94078302b6fd9a27aa5dc57dfe9d
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-06-11 23:50:27 -0300

    Tipo Float: double

---
+++ java-JRE-JDK/syntax-variables-and-flow/src/VariableFloat.java
@@ -0,0 +1,18 @@
+public class VariableFloat {
+
+    public static void main(String[] args) {
+        double salario = 1250.50;
+
+        System.out.println("O salário é: " + salario);
+
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+
+        int outraDivisao = 5 / 2; // 2
+        System.out.println(outraDivisao);
+
+        double novaDivisao = 5.0 / 2; // 2.5
+        System.out.println(novaDivisao);
+    }
+
+}
