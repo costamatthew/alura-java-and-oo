2ff7a73a58039f998be4ff300ced38da77f3cb93
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-06-12 21:10:14 -0300

    Char and String

---
+++ java-JRE-JDK/syntax-variables-and-flow/src/VariableString.java
@@ -0,0 +1,20 @@
+public class VariableString {
+
+    public static void main(String[] args) {
+        char letra = 'a';
+        System.out.println(letra);
+
+        char valor = 66; // B
+        System.out.println(valor);
+
+        valor = (char) (valor + 1); // C
+        System.out.println(valor);
+
+        String palavra = "guardando uma frase";
+        System.out.println(palavra);
+
+        palavra = palavra + 2020;
+        System.out.println(palavra);
+    }
+
+}
