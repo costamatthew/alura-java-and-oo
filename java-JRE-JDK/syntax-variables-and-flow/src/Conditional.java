8e6e181983cf7893aa19fc5021da9ebccd49a5c6
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-06-15 23:34:07 -0300

    feat: create first conditional in java

---
+++ java-JRE-JDK/syntax-variables-and-flow/src/Conditional.java
@@ -0,0 +1,15 @@
+public class Conditional {
+
+    public static void main(String[] args) {
+        System.out.println("Testando Condicionais");
+
+        int idade = 20;
+
+        if (idade >= 18) {
+            System.out.println("Você tem mais de 18 anos");
+        } else {
+            System.out.println("Infelismente voce nao pode entrar");
+        }
+    }
+
+}
