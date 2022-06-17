9143ac795fffd0ed42c41bdf20d324ebc0d61e99
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-06-17 01:39:29 -0300

    boolean conditional

---
+++ java-JRE-JDK/syntax-variables-and-flow/src/Boolean.java
@@ -0,0 +1,16 @@
+public class Boolean {
+
+    public static void main(String[] args) {
+        System.out.println("Testando Condicionais");
+
+        int idade = 23;
+        boolean acompanhado = true;
+
+        if (idade >= 18 && acompanhado) {
+            System.out.println("Seja bem vindo");
+        } else {
+            System.out.println("Infelismente voce nao pode entrar");
+        }
+    }
+
+}
